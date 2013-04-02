de81fec199efc966a7a039f229a51499bf29cb01
Matteo Rosati <rosati.matteo@gmail.com>
2013-04-02 20:56:04 +0200

    added south and picturemanager to apps

--- stockmanager/settings/base.py
+++ stockmanager/settings/base.py
@@ -106,10 +106,12 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    'south',
     'django.contrib.admin',
     'django_extensions',
 
     'stockmanager',
+    'picturemanager',
 )
 
 # A sample logging configuration. The only tangible logging
