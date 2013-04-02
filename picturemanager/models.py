17986b49cbbe727812c075f548b5259c26ff71ea
Matteo Rosati <rosati.matteo@gmail.com>
2013-04-02 21:15:08 +0200

    model and migrations

--- picturemanager/models.py
+++ picturemanager/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Picture(models.Model):
+	name = models.CharField(max_length=255)
+	author = models.ForeignKey(User)
+	projects = models.ManyToManyField('Project')
+	uploaded = models.DateTimeField(auto_now=True)
+	picture = models.ImageField(upload_to='pictures')
+	exif = models.TextField(blank=True, null=True)
+
+class Project(models.Model):
+	name = models.CharField(max_length=255)
+
+	def __unicode__(self):
+		return self.name
